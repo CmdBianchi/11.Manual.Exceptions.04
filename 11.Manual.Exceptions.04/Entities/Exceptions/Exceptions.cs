1b1c1a043786b249b1e5c81d9ecc87d161f3a37d
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-15 17:50:11 -0300

    Adicionar arquivos de projeto.

---
+++ 11.Manual.Exceptions.04/Entities/Exceptions/Exceptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities.Exceptions {
+    class DomainException : ApplicationException {
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
---
+++ 11.Manual.Exceptions.04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Entities.Exceptions;
+using Entities;
+namespace _11.Manual.Exceptions._04 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: "); int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: "); string holder = Console.ReadLine();
+            Console.Write("Initial balance: "); double balance = double.Parse(Console.ReadLine());
+            Console.Write("Withdraw limit: "); double withdrawLimit = double.Parse(Console.ReadLine());
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter amount for withdraw: "); double amount = double.Parse(Console.ReadLine());
+            try {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2"));
+            }
+            catch (DomainException e) {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
